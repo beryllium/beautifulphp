3496e0c4d6c0392a3ec19d753511c112a6659f69
Kevin Boyd <kevin.boyd@gmail.com>
2016-10-06 10:37:17 -0700

    Converting to composer.json instead of sculpin.json

---
+++ app/SculpinKernel.php
@@ -0,0 +1,12 @@
+<?php
+
+class SculpinKernel extends \Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel
+{
+    protected function getAdditionalSculpinBundles()
+    {
+        return array(
+            'Beryllium\Icelus\IcelusBundle',
+        );
+    }
+}
+
